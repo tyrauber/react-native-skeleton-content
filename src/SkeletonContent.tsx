dd0713f4171741fb941e6b75d7025b38f15be27e
Ty Rauber <tyrauber@mac.com>
2022-12-06 07:21:10 -0400

    Fix: update dependencies to peerDependencies - use react-native-linear-gradient instead of expo-linear-gradient as the later is dependent on expo-core-modules

--- src/SkeletonContent.tsx
+++ src/SkeletonContent.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { LayoutChangeEvent, StyleSheet, View } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import LinearGradient from 'react-native-linear-gradient';
 import Animated, { interpolateNode } from 'react-native-reanimated';
 import {
   interpolateColor,
